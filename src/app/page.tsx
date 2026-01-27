7bc308bc976b3cf9f35dd71076a59e3bf78ea2a9
Celestine Assumpta <assumptachinonso42@gmail.com>
2026-01-27 18:06:40 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 
+
+import React from "react";
+import Link from "next/link";
+
  export default function HomePage() {
+  
     return (
-        <div style={{ minHeight: "80vh", padding: "20px", background: "#66ccff",
+        <div style={{ minHeight: "100vh", padding: "20px", background: "#66ccff",
           display: "flex", alignItems: "center", justifyContent: "center", 
         }}>
            <section style={{ maxWidth: "600px", margin: "0 auto" }}>
@@ -11,11 +16,12 @@
                     This is the main landing page of your dashboard application. Use the sidebar to navigate through different sections such as Courses, Settings, and more.
                 </p>
             </div>
-
-            <button style={{ padding: "10px 20px", fontSize: "1rem", backgroundColor: "#007bff", color: "white", border: "none", 
+            <br />
+                  
+          <Link href="/projects" style={{ padding: "10px 20px", fontSize: "1rem", backgroundColor: "#007bff", color: "white", border: "none", 
               borderRadius: "5px", cursor: "pointer", gap: "10px", marginTop: "20px" }}>
             Get Started
-          </button>
+          </Link>
             </section>
 
          
