44c05cbc8445790a2f2f36209edb80b557d12a40
Celestine Assumpta <assumptachinonso42@gmail.com>
2026-01-28 16:33:55 +0100

    had to put back the not found page because it is required by nextjs routing system

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+
+
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-black/5">
+      <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+      <p className="text-lg text-gray-700">
+        The page you are looking for does not exist.
+      </p>
+    </div>
+  );
+}
