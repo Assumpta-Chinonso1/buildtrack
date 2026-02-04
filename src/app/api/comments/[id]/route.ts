fdd02fb04aee36657f77f974771100d79b40208b
Celestine Assumpta <assumptachinonso42@gmail.com>
2026-02-04 13:04:31 +0100

    4/2/2026

--- src/app/api/comments/[id]/route.ts
+++ src/app/api/comments/[id]/route.ts
@@ -1,20 +1,45 @@
-export const runtime = "nodejs";
-
+import { request } from "http";
 import { comments } from "../data";
 
 export async function GET(
   _request: Request,
-  { params }: { params: { id: string } }
-) {
-  const id = parseInt(params.id);
+   {params } : {params: Promise<{id: string}>})
 
-  const comment = comments.find(
-    (comment) => comment.id === id
-  );
+   {
+    const {id} = await params;
+    const comment = comments.find((comment) => comment.id === parseInt(id))
 
-  if (!comment) {
-    return new Response("Comment not found", { status: 404 });
-  }
+    return Response.json(comment)
+   }
 
-  return Response.json(comment);
-}
+   export async function PATCH(
+  request: Request,
+   {params } : {params: Promise<{id: string}>})
+
+   {
+    const{id} = await params
+    const body = await request.json()
+    const {text } = body;
+
+    const index = comments.findIndex((comment) => comment.id === parseInt(id))
+
+    comments[index].text = text
+
+
+
+    return Response.json(comments[index])
+   }
+
+    export async function DELETE(
+  _request: Request,
+   {params } : {params: Promise<{id: string}>})
+
+   {
+           const {id} = await params;
+           const index = comments.findIndex((comment) => comment.id === parseInt(id))
+            
+           const deletedComment = comments[index]
+           comments.splice(index, 1)
+
+           return Response.json(deletedComment)
+   }
--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,4 +1,11 @@
-export async function GET() {
+import { type NextRequest } from "next/server"
+
+export async function GET(request: NextRequest) {
+
+  const requestHeaders = new Headers(request.headers)
+
+  console.log(requestHeaders.get("Authoriasation"))
+
     return new Response("Api profile Page")
     
 }
