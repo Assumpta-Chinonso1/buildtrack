f702c1e737cb1159a8692b6a011a838001b3235d
Celestine Assumpta <assumptachinonso42@gmail.com>
2026-02-10 13:06:09 +0100

    fetching data in Client component

---
+++ src/app/users-client-page/page.tsx
@@ -0,0 +1,63 @@
+"use client"
+import { useEffect, useState } from "react";
+
+type User = {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    phone: string
+}
+
+export default function UsersClientPage() {
+
+    const [users, setUsers] = useState<User[]> ([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState("")
+
+    useEffect(() => {
+        async function fetchUser() {
+            try {
+                const response = await fetch(
+                    "https://jsonplaceholder.typicode.com/users"
+                )
+                if(!response.ok) throw new Error("faild to fetch users");
+                const data = await response.json()
+                setUsers(data)
+
+            } catch (err) {
+                if(err instanceof Error){ 
+                setError(err.message)
+            } else {
+                setError("An Unknown erro occurred")
+            }
+        } finally{
+            setLoading(false)
+        }
+        }
+        fetchUser()
+    }, [])
+
+    if(loading) return <div>Loading...</div>
+    if(error) return <div>{error}</div>
+
+    return (
+
+        <ul className="space-y-4 p-4">
+                   {users.map((user) => (
+                    <li key={user.id}
+                    className="p-2   bg-white shadow-md rounded-lg  text-gray-700 ">
+
+                        <div className="font-bold">{user.name}</div>
+                        <div className="text-sm">
+                          <div>Username: {user.username}</div>
+                          <div>Email:  {user.email} </div>
+                          <div>Phone: {user.phone} </div>
+                        </div>
+                        
+
+                    </li>
+                   ))}
+        </ul>
+    )
+}
