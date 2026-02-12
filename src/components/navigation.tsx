73154ff709ce2ab20760dfff79a8c4b6dbf13ae4
Celestine Assumpta <assumptachinonso42@gmail.com>
2026-02-12 16:38:25 +0100

    profile setting using clerk

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,19 +1,27 @@
 
-import { SignInButton, SignOutButton } from '@clerk/nextjs';
+import { SignInButton, 
+    //UserButton 
+     SignOutButton } from '@clerk/nextjs';
+import Link from 'next/link';
 
 export const Navigation = () => {
   return (
     <nav className="bg-[var(--background)] border-b border-[var(--foreground)]/10">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16 items-center">
+      <div className="max-w-9xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-17 items-center">
           <div className="flex-shrink-0">
             <h1 className="text-xl font-semibold text-[var(--foreground)]">
                 BuildTrack
                 </h1>
           </div>
-          {/*Sign in botton goes here*/}
+          <div className="flex items-center gap-4">
+               {/*Sign in botton goes here*/}
           <SignInButton mode="modal"/>
+         {/* <UserButton/>*/}
+          <Link href="/user-profile">Profile</Link>
             <SignOutButton />
+          </div>
+          
         </div>
       </div>
     </nav>
